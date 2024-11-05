3494fcd46c7e8886cd371ec2c61b9705a3212eb8
Vivekachooz <vivekachooz@gmail.com>
2024-11-06 01:59:22 +0530

    oscaro: Import initial device specific blobs
    * From CPH2381_14.0.0.920(EX01) build
    
    Change-Id: I6503c4191738ddd77c9b10c4094ad256bda8510a

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),oscaro)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,4fd2e8c805e37b15795e1da02736fd19582c3257)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,b6ab65bab8abd985d2c41438351581120a7050fd)
+$(call add-radio-file-sha1-checked,radio/core_nhlos.img,444b30ece461f9ba17122e4727e3de42266a3d78)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,883ac3162a1058c824ce53b9e84d7ca306bdab92)
+$(call add-radio-file-sha1-checked,radio/dsp.img,9606e98a9e1a1b886170e4858e7dc5ab2910548a)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,a52fad862c855ba724bc57f6ab942193187ff128)
+$(call add-radio-file-sha1-checked,radio/hyp.img,b5d8a9940b4af25845d55e4ebfe7f5a8d00dddc5)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,442f462892fcc8cae89b76f566e3da3cee0c0b29)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,75965ad6bd325ef1516a517eaceb5484b9ac6335)
+$(call add-radio-file-sha1-checked,radio/modem.img,d56ad7a6288f7f73daf21a4c013fd8acd6b7c68f)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,611818027635a560cf56f608943533a9560a7c50)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,abb5657844831795ddb96c47d161404653e98e37)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,d29f1af165e2d058ab85090248698a83104355ca)
+$(call add-radio-file-sha1-checked,radio/rpm.img,90ec7903467c638bf743f271f55f855da5fc1afe)
+$(call add-radio-file-sha1-checked,radio/splash.img,cef1d4886175660f305e78ab39b7782f8625e431)
+$(call add-radio-file-sha1-checked,radio/tz.img,8a51f3d0d6c1a3d465ce39ec1db87f31013b2db2)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,2e8077c6faf2531241bb2df856ee964823f64a97)
+$(call add-radio-file-sha1-checked,radio/xbl.img,0eb75aef5086005ab8dc92424a17e3b227a887ad)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,de36ea870620d7ce0c318bfa78ddf944440cdfa5)
+
+endif
